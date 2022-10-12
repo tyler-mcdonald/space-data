70be086a0558f2e2a0df24e6fdfc40d2ce042a53
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-12 15:22:48 -0500

    Refactor fetchData

--- src/App.js
+++ src/App.js
@@ -7,29 +7,27 @@ function App() {
   const [launchData, setLaunchData] = useState(null);
   const [rocketData, setRocketData] = useState(null);
 
+  async function fetchData(url, setState) {
+    const response = await fetch(url);
+    const data = await response.json();
+    setState(data);
+  }
+
   // Get launch data
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(
-        "https://api.spacexdata.com/v4/launches/5eb87cd9ffd86e000604b32a"
-      );
-      const data = await response.json();
-      setLaunchData(data);
-    }
-    fetchData();
+    fetchData(
+      "https://api.spacexdata.com/v4/launches/5eb87cddffd86e000604b32f",
+      setLaunchData
+    );
   }, []);
 
   // Get rocket data
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(
-        `https://api.spacexdata.com/v4/rockets/${launchData.rocket}`
-      );
-      const data = await response.json();
-      setRocketData(data);
-    }
     if (launchData) {
-      fetchData();
+      fetchData(
+        `https://api.spacexdata.com/v4/rockets/${launchData.rocket}`,
+        setRocketData
+      );
     }
   }, [launchData]);
 
