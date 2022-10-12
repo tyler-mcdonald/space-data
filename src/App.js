63ffd9017a416d7c2039db22acb199b012ebce47
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-11 21:55:11 -0500

    Connect react client to API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// deps
+import { useState, useEffect } from "react";
+// components
+import { LaunchInfo } from "./components/LaunchInfo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  const fetchData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(await data);
+  };
+
+  useEffect(() => {
+    fetchData("https://ll.thespacedevs.com/2.2.0/launch/");
+  }, []);
+
+  return <>{data ? <LaunchInfo data={data.results} /> : null}</>;
 }
 
 export default App;
---
+++ src/components/LaunchInfo.js
@@ -0,0 +1,13 @@
+export const LaunchInfo = ({ data }) => {
+  const launch = data[0];
+  // image
+  // craft
+  // mission description
+  // mission date
+  // mission location
+  // mission status (success/fail)
+  // status message
+  //
+
+  return <img src={launch.image} alt="_________" />;
+};
