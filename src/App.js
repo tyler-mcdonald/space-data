a48906f0a418745e455d92043095ff5a67bf9795
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-12 13:04:37 -0500

    Skeleton display of launch data

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import { LaunchInfo } from "./components/LaunchInfo";
 function App() {
   const [data, setData] = useState(null);
 
+  useEffect(() => {
+    fetchData(
+      "https://api.spacexdata.com/v4/launches/5eb87cd9ffd86e000604b32a"
+    );
+  }, []);
+
   const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     setData(await data);
+    console.log(await data);
   };
 
-  useEffect(() => {
-    fetchData("https://ll.thespacedevs.com/2.2.0/launch/");
-  }, []);
-
-  return <>{data ? <LaunchInfo data={data.results} /> : null}</>;
+  return <>{data ? <LaunchInfo launch={data} /> : null}</>;
 }
 
 export default App;
--- src/components/LaunchInfo.js
+++ src/components/LaunchInfo.js
@@ -1,13 +1,50 @@
-export const LaunchInfo = ({ data }) => {
-  const launch = data[0];
-  // image
-  // craft
-  // mission description
-  // mission date
-  // mission location
-  // mission status (success/fail)
-  // status message
+export const LaunchInfo = ({ launch }) => {
+  // date
+  // rocket (pull from other API)
+  // success/failure
+  // mission details
+  // mission article
+  // mission youtube id
   //
 
-  return <img src={launch.image} alt="_________" />;
+  return (
+    <>
+      <div>
+        <p>Date: {launch.date_local}</p>
+      </div>
+
+      <div>
+        <p>
+          Mission {launch.success ? "Success" : "Failure"}: {launch.details}
+        </p>
+      </div>
+
+      <div>
+        <img src={launch.links.patch.small} alt="mission patch" />
+      </div>
+
+      <div>
+        <p>Rocket</p>
+        <p>#</p>
+      </div>
+
+      <div>
+        <a
+          href={`https://www.youtube.com/watch?v=${launch.links.youtube_id}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Watch on Youtube
+        </a>
+      </div>
+
+      <br></br>
+
+      <div>
+        <a href={launch.links.article} target="_blank" rel="noreferrer">
+          Read Article
+        </a>
+      </div>
+    </>
+  );
 };
