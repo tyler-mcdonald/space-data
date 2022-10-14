a401269d48e9e7ab08255681ee5648298ebdb667
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-14 17:35:26 -0500

    create RandomLaunch component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import { useState, useEffect } from "react";
-import { LaunchInfo } from "./components/LaunchInfo";
+// components
+import { RandomLaunch } from "./components/RandomLaunch";
 
 function App() {
-  const [launchData, setLaunchData] = useState(null);
-  const [rocketData, setRocketData] = useState(null);
-  const randomLaunch = launchData
-    ? launchData[Math.floor(Math.random() * launchData.length)]
-    : null;
-
-  // Set launch data
-  useEffect(() => {
-    fetchData("https://api.spacexdata.com/v4/launches", setLaunchData);
-  }, []);
-
-  // Get rocket data
-  useEffect(() => {
-    if (launchData) {
-      fetchData(
-        `https://api.spacexdata.com/v4/rockets/${randomLaunch.rocket}`,
-        setRocketData
-      );
-    }
-  }, [launchData]);
-
-  async function fetchData(url, setState) {
-    const response = await fetch(url);
-    const data = await response.json();
-    setState(data);
-  }
-
-  return (
-    <>
-      {launchData && rocketData ? (
-        <LaunchInfo launch={randomLaunch} rocket={rocketData} />
-      ) : null}
-    </>
-  );
+  return <RandomLaunch />;
 }
 
 export default App;
