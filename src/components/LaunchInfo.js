d646c3dae0565e1fa43e89c14d514805159ba10d
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-14 16:26:16 -0500

    add date formatting

--- src/components/LaunchInfo.js
+++ src/components/LaunchInfo.js
@@ -6,17 +6,41 @@ export const LaunchInfo = ({ launch, rocket }) => {
   // mission article
   // mission youtube id
   //
-  const { date_local, success, details, links } = launch;
+  const { date_utc, success, details, links } = launch;
+
+  const formatDate = (date) => {
+    // "2006-03-24T22:30:00.000Z"
+    const year = date.slice(0, 4);
+    const month = date.slice(5, 7);
+    const day = date.slice(8, 10);
+    const time = date.slice(11, 16);
+
+    function toMonthName(monthNumber) {
+      const date = new Date();
+      date.setMonth(monthNumber - 1);
+      return date.toLocaleString("en-US", {
+        month: "long",
+      });
+    }
+
+    const monthName = toMonthName(month);
+
+    // format date
+    const formattedDate = `${monthName} ${day}, ${year}`;
+    return formattedDate;
+  };
 
   return (
-    <>
+    <section className="flex">
       <div>
-        <p>Date: {date_local}</p>
+        <p>Launch: {formatDate(date_utc)}</p>
+        <p></p>
       </div>
 
       <div>
         <p>
-          Mission {success ? "Success" : "Failure"}: {details}
+          Mission {success ? "Success" : "Failure"}
+          {details ? `: ${details}` : ""}
         </p>
       </div>
 
@@ -41,10 +65,13 @@ export const LaunchInfo = ({ launch, rocket }) => {
       <br></br>
 
       <div>
-        <a href={links.article} target="_blank" rel="noreferrer">
-          Read Article
-        </a>
+        {/* check if article exists */}
+        {links.article ? (
+          <a href={links.article} target="_blank" rel="noreferrer">
+            Read Article
+          </a>
+        ) : null}
       </div>
-    </>
+    </section>
   );
 };
