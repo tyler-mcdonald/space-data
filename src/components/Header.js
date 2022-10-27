8d857b1089bf75ce91bce8d67f0f99fb94073e94
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-10-27 15:27:03 -0500

    create basic navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <div className="title">
-          <h3>SpaceX Launch Data</h3>
-        </div>
-        <div className="nav">
-          <a href="/">Random Launch</a>
-        </div>
+    <nav className="navbar">
+      <div className="brand-title">SpaceX Launches</div>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/launches/upcoming">Upcoming Launches</Link>
+          </li>
+          <li>
+            <Link to="/launches/past">Past Launches</Link>
+          </li>
+          <li>
+            <Link to="/launches/data">Detailed Launch Data</Link>
+          </li>
+        </ul>
       </div>
-    </header>
+    </nav>
   );
 };
