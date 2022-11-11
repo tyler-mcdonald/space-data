0b14961a8eaabbbc3fd68c335b235c839dcb0f61
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-11-11 17:43:04 -0600

    fetch all launches

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,56 +1,17 @@
 import { useEffect, useState } from "react";
 
 export const Countdown = () => {
-  const [launch, setLaunch] = useState(null);
-  const [timeRemaining, setTimeRemaining] = useState(0); // initial????
+  const [launchData, setLaunchData] = useState(null);
 
   useEffect(() => {
-    const data = fetchData("https://api.spacexdata.com/v4/launches/next");
-    setState(data, setLaunch);
+    getLaunchData();
   }, []);
 
-  async function fetchData(url) {
-    const response = await fetch(url);
+  async function getLaunchData() {
+    const response = await fetch("https://api.spacexdata.com/v4/launches");
     const data = await response.json();
-    return data;
+    setLaunchData(data);
   }
 
-  async function setState(state, setState) {
-    setState(await state);
-  }
-
-  // calculate time until launch
-  async function getTimeRemaining() {
-    const currentTime = new Date().getTime();
-    const launchTime = new Date(await launch.date_utc).getTime();
-    const timeRemaining = launchTime - currentTime;
-    // format
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)); // second * minute * hour * day
-    const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24); // (sec * min * hr)
-    const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
-    const seconds = Math.floor((timeRemaining / 1000) % 60);
-    return { days, hours, minutes, seconds };
-  }
-  console.log(getTimeRemaining());
-
-  function updateCountdown() {
-    // update timer state each second
-    setInterval(() => {
-      const t = getTimeRemaining();
-      setTimeRemaining(t);
-    }, 1000);
-    // make sure to clean up
-  }
-
-  // update until launch happens
-  // when launch happens
-  //      get next launch
-
-  return (
-    <div>
-      <h1>
-        <p>{`${timeRemaining.days}:${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</p>
-      </h1>
-    </div>
-  );
+  console.log(launchData);
 };
