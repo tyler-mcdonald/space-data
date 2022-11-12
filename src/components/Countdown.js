325d79ae5ea9a7068e6b96c429a2d1fa67cfded4
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-11-12 12:47:06 -0600

    set nextLaunch state

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,17 +1,35 @@
 import { useEffect, useState } from "react";
 
 export const Countdown = () => {
-  const [launchData, setLaunchData] = useState(null);
+  const [launchData, setLaunchData] = useState([]);
+  const [nextLaunch, setNextLaunch] = useState(null);
 
   useEffect(() => {
+    async function getLaunchData() {
+      const response = await fetch("https://api.spacexdata.com/v4/launches");
+      const data = await response.json();
+      setLaunchData(data);
+    }
     getLaunchData();
   }, []);
 
-  async function getLaunchData() {
-    const response = await fetch("https://api.spacexdata.com/v4/launches");
-    const data = await response.json();
-    setLaunchData(data);
+  useEffect(() => {
+    setNextLaunch(getNextLaunch());
+  }, [launchData]);
+
+  function getNextLaunch() {
+    const now = new Date().getTime() / 1000;
+    let nextLaunch;
+
+    for (let launch of launchData) {
+      if (launch.date_unix > now) {
+        nextLaunch = launch;
+        break;
+      }
+    }
+
+    return nextLaunch;
   }
 
-  console.log(launchData);
+  console.log(nextLaunch);
 };
